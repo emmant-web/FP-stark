dfc4bda63d5259ab6bfbc7167c9447d2b9b953f1
emmant-web <emmanueldtoribio@gmail.com>
2026-02-11 09:11:22 +0800

    finished setting up the base ui system

---
+++ src/pages/LibraryPage.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import Body from "../components/body/Body";
+import Footer from "../components/Footer";
+
+function LibraryPage() {
+  return (
+    <div className="dvh-screen">
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default LibraryPage;
